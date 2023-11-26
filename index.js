d2966568cb1d829b510ed99792b196d13a24ecbb
quangtruong3011 <lequangtruong3011@gmail.com>
2023-11-26 18:16:14 +0700

    update

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import { errorHandlerMiddleware } from "./src/middlewares/error.middleware.js";
 
 const app = express();
 
-const whitelist = ["http://localhost:3000"];
+const whitelist = ["https://admin-dasboard.onrender.com"];
 
 const corsOptions = {
     origin: function (origin, callback) {
@@ -22,7 +22,7 @@ const corsOptions = {
 connectToDatabase();
 
 app.use(express.json());
-app.use(cors("*"));
+app.use(cors(corsOptions));
 
 app.use("/api", router);
 
